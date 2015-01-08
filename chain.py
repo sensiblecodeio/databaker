379d38182c34edb7da16e1623294bac531003f6d
Dragon Dave McKee <dragon@scraperwiki.com>
2015-01-08 14:17:37 +0000

    chain

--- chain.py
+++ chain.py
@@ -1,13 +1,15 @@
+import functools
+
 class Chain(object):
-    def invoke(self, *args, **kwargs):
+    def invoke(self, __sw_name, *args, **kwargs):
         print "args", args, kwargs
         chain = list(self.chain)
-        chain.append([args, kwargs])
+        chain.append([__sw_name, args, kwargs])
         return Chain(self.target, chain=chain) # TODO
 
     def missing(self, name):
         # chain.append(name)
-        return self.invoke
+        return functools.partial(self.invoke, name)
 
     def __init__(self, target, chain=None):
         self.target = target
@@ -18,16 +20,23 @@ class Chain(object):
         return self.missing(name)
 
     def __repr__(self):
-        return "Chain({!r}, {!r})".format(self.target, self.chain)
+        return "Chain({}, {!r})".format(self.target.__name__, self.chain)
 
 
-sheet = Chain(target='sheet_object')
+
+def sheet_object(*args, **kwargs):
+    print args, kwargs
+
+sheet = Chain(target=sheet_object)
 down = 1
 right = 2
 
 def is_number(n):
     return n == 55
-q = Chain('a').b1('b2').c1('c2')# .ocelot
-print q
+q = Chain(sheet_object).a1('a2').b1('b2')# .ocelot
+print q, type(q)
+
+z = q.foo
+print z, type(z)
 exit()
 
