cffd16eb65849cc396e72c5f234ef8de46faf04d
Dragon Dave McKee <dragon@scraperwiki.com>
2015-01-13 11:37:05 +0000

    more a02

--- a02.py
+++ a02.py
@@ -4,13 +4,13 @@ import atexit
 import xypath
 
 # NOTE shim
-def col(sheet, column):
-    if isinstance(column, basestring):
-        raise NotImplementedError  # TODO
-    else:
-        assert isinstance(column, int)
-        return sheet._x_index[column]
-xypath.Table.col = col
+
+from xypath import DOWN, RIGHT, UP, LEFT
+xypath.Bag.regex = lambda self, x: self.filter(re.compile(x))
+
+def one_of(bag, options):
+    return bag.filter(lambda cell: cell.value in options)#
+xypath.Bag.one_of = one_of
 
 def showtime(msg='unspecified'):
     global last
@@ -33,31 +33,52 @@ import xypath
 sheet = xypath.Table.from_filename('resource/table-a02.xls', table_name='seasonally adjusted')
 showtime("file imported")
 
-obs = sheet.filter("MGSL").assert_one().shift(xypath.DOWN).fill(xypath.RIGHT).fill(xypath.DOWN).filter(is_number)
+obs = sheet.filter("MGSL").assert_one().shift(DOWN).fill(RIGHT).fill(DOWN).filter(is_number)
 # note: this is MUCH faster than DOWN/RIGHT
 showtime("got obs")
 
-genders = sheet.col(0).filter(lambda x: x.value in ['Male', 'Female', 'All Persons'])
+genders = sheet.col('A').one_of(['Male', 'Female', 'All Persons'])
 showtime("got genders")
 
-times = sheet.col(0).fill(xypath.DOWN).filter(re.compile("...-... (?:19|20)\d\d"))
+times = sheet.col('A').fill(DOWN).regex("...-... (?:19|20)\d\d")
 showtime("got times")
 
-ages = sheet.filter(re.compile("All aged .*")) # surprised this is fast!
+ages = sheet.regex("All aged .*") # surprised this is fast!
 showtime("all_age_labels")
 
-indicators = sheet.filter("Total economically active").fill(xypath.LEFT).fill(xypath.RIGHT)
+indicators = sheet.filter("Total economically active").fill(LEFT).fill(RIGHT)
 showtime("indicators")
 
-for i, ob in enumerate(obs):
-    if i% 283 == 0:
+def for_loop():
+    for i, ob in enumerate(obs.unordered_cells):
         out = {}
-        out['ob'] = ob.value
-        out['gender'] = ob.lookup(genders, xypath.UP)
-        out['time'] = ob.lookup(times, xypath.LEFT, strict=True)
-        out['age'] = ob.lookup(ages, xypath.UP)
-        out['indicator'] = ob.lookup(indicators, xypath.UP, strict=True)
-        print out
+        out['ob'] = ob
 
-# TODO filter
+        out['gender'] = ob.lookup(genders, UP)
+        out['time'] = ob.lookup(times, LEFT, strict=True)
+        out['age'] = ob.lookup(ages, UP)
+        out['indicator'] = ob.lookup(indicators, UP, strict=True)
+        #out['geographic'] = 'UK'
+        #out['statistical_unit'] = 'People'
+        mystical_output_function(out)
 
+def mystical_output_function(p):
+    for item in p:
+        print p[item].value,
+    print
+
+for_loop()
+
+def magic_loop():
+    presto = Magic(obs)
+    presto.label("genders", UP)
+    presto.evaluate()
+
+def single_iteration(ob):
+    out = {}
+    out['ob'] = ob.value
+    out['gender'] = ob.lookup(genders, UP)
+    out['time'] = ob.lookup(times, LEFT, strict=True)
+    out['age'] = ob.lookup(ages, UP)
+    out['indicator'] = ob.lookup(indicators, UP, strict=True)
+    return out
