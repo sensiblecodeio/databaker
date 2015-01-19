c037ba427a2a5c91857b6b60b798bdef84bfc280
Dragon Dave McKee <dragon@scraperwiki.com>
2015-01-19 17:27:03 +0000

    dimensions, also remove empty indicators

--- a02.py
+++ a02.py
@@ -8,8 +8,8 @@ def per_sheet(sheet):
     # note: this is MUCH faster than DOWN/RIGHT
     showtime("got obs")
 
-    sheet.col('A').one_of(['Male', 'Female', 'All Persons']).is_header('gender', UP)
+    sheet.col('A').one_of(['Male', 'Female', 'All Persons']).is_header('gender', UP, dim=1)
     sheet.col('A').fill(DOWN).regex("...-... (?:19|20)\d\d").is_header('time', LEFT, strict=True)
-    sheet.regex("All aged .*").is_header('ages', UP)
-    sheet.filter("Total economically active").fill(LEFT).fill(RIGHT).is_header('indicator', UP, strict=True)
+    sheet.regex("All aged .*").is_header('ages', UP, dim=2)
+    sheet.filter("Total economically active").fill(LEFT).fill(RIGHT).filter(lambda x: x.value).is_header('indicator', UP, strict=True)
     return obs
