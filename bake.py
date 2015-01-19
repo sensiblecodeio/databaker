6a1cb0abaec6a608fbe12be57636a77427911d61
Dragon Dave McKee <dragon@scraperwiki.com>
2015-01-19 17:28:20 +0000

    rubbish CSV output

--- bake.py
+++ bake.py
@@ -10,17 +10,36 @@ from timeit import default_timer as timer
 import atexit
 import xypath
 import xypath.loader
+from utf8csv import UnicodeWriter
+import bake
 
+class FakeCell(object):
+    value = ''  # cell isn't there
+
+csv_filehandle = None
+dims = {'time': 19, 'indicator': 5, 'ob': 1}
+maxcol = 43
+revdims = None
 def cell_repr(cell):
     column = xypath.contrib.excel.excel_column_label(cell.x+1)
     return "<{}{!r} {}>".format(column, cell.y+1, cell.value)
 xypath.xypath._XYCell.__repr__ = cell_repr
 
-def is_header(bag, name, *args, **kwargs):
+def update_dim(name, col):
+    print name, col
+    global dims, maxcol
+    assert isinstance(col, int)
+    dims[name] = 29 + (col*8)
+    maxcol = max(maxcol, (35+col*8))
+    print dims
+
+def is_header(bag, name, direction, dim=None, *args, **kwargs):
+    if dim:
+        bake.update_dim(name, dim) # dim given is one-indexed - see ONS CSV spec.
     if getattr(bag.table, 'headers', None) is None:
         bag.table.headers = {}
-    print name, "cell.lookup({}, *{}, **{}".format(bag, args, kwargs)
-    bag.table.headers[name] = lambda cell: cell.lookup(bag, *args, **kwargs)
+    print name, "cell.lookup({}, {}, dim={}, *{}, **{}".format(bag, direction, dim, args, kwargs)
+    bag.table.headers[name] = lambda cell: cell.lookup(bag, direction, *args, **kwargs)
     showtime("got header {}".format(name))
 xypath.Bag.is_header = is_header
 
@@ -58,6 +77,15 @@ def single_iteration(ob, **foo):
             #raise
     return out
 
+
+def rooooow(row):
+    return [row.get(revdims.get(i, None), FakeCell) for i in range(1, bake.maxcol+1)]
+
+
+def csv_output(row):
+    csv_writer.writerow([unicode(outcell.value) for outcell in rooooow(row)])
+
+
 if __name__ == '__main__':
     __version__ = "0.0.0"
     options = docopt(__doc__, version='databaker {}'.format(__version__))
@@ -65,12 +93,17 @@ if __name__ == '__main__':
     recipe_file = options['<recipe>']
     recipe = imp.load_source("recipe", recipe_file)
     #filenames = ['resource/table-a02.xls']  # will have come from command line glob
-    for fn in filenames:
-        print fn
-        tableset = xypath.loader.table_set(fn)
-        showtime("file imported")
-        for sheet in xypath.loader.get_sheets(tableset, recipe.per_file(tableset)):
-            showtime("sheet imported")
-            obs = recipe.per_sheet(sheet)
-            for ob in obs:
-                output_row=single_iteration(ob)
+    with open("out.csv", "w") as csv_filehandle:
+        csv_writer = UnicodeWriter(csv_filehandle)
+        for fn in filenames:
+            print fn
+            tableset = xypath.loader.table_set(fn)
+            showtime("file imported")
+            for sheet in xypath.loader.get_sheets(tableset, recipe.per_file(tableset)):
+                showtime("sheet imported")
+                obs = recipe.per_sheet(sheet)
+                revdims = {pos: name for name, pos in bake.dims.items()}
+                for ob in obs:
+                    output_row=single_iteration(ob)
+                    csv_output(output_row)
+
