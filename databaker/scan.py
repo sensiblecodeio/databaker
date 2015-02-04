6c2350da2125a04cf94e8d524dd677cc8c47ac76
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-04 15:33:28 +0000

    scan for repeated dimensions [hacky]

---
+++ databaker/scan.py
@@ -0,0 +1,15 @@
+dims = [14, 17, 38, 46, 54, 62, 70]
+
+from utf8csv import UnicodeReader
+
+seen = set()
+with open('out.csv', 'r') as f:
+    for i, item in enumerate(UnicodeReader(f)):
+        thisrow = []
+        for dim in dims:
+            try:
+                thisrow.append(item[dim])
+            except IndexError:
+                break
+        seen.add('!'.join(thisrow))
+        assert len(seen) == i + 1, (thisrow)
