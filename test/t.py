a6c9f642198ac0058d38e2127b7c4b10d69b8afe
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-18 11:58:51 +0000

    more tests

--- test/t.py
+++ test/t.py
@@ -2,17 +2,19 @@ import databaker.bake
 from databaker.constants import *
 from nose.tools import assert_equal
 def per_file(tabs):
-    return "*"
+    return tabs.names
 
 
 def per_tab(tab):
     tab.dimension("header_1", "static_value")
-    tab.excel_ref("A1").fill(RIGHT).is_not_blank().dimension('header_2', DIRECTLY, ABOVE)
+    tab.excel_ref("A1").fill(RIGHT).is_not_blank().dimension(TIME, DIRECTLY, ABOVE)
     obs = tab.excel_ref("A1").fill(RIGHT).is_not_blank().fill(DOWN).is_not_blank()
-    assert_equal(tab.excel_ref("A6"), tab.get_at(0,5))
+
+
     assert_equal(tab.filter('merged'), tab.filter('mergeanchor').shift(UP).parent())
     assert_equal(tab.filter('merged'), tab.excel_ref("F2"))
 
+    assert_equal(tab.excel_ref("A6"), tab.get_at(0,5))
     tab.excel_ref("A").filter("a3").assert_one()
     tab.excel_ref("3").filter("a3").assert_one()
     tab.excel_ref("A3").filter("a3").assert_one()
@@ -22,6 +24,14 @@ def per_tab(tab):
     tab.excel_ref("2:4").filter("c3").assert_one()
     tab.excel_ref("A2:C4").filter("b3").assert_one()
 
+    counting = tab.filter("rabbit").fill(RIGHT).is_number()
+    assert not counting.filter("sir")
+    assert counting.filter(lambda x: x.value == 5)
+    assert_equal(len(counting), 5)
+
+    assert_equal(tab.filter("rabbit").group("(a.*i)"), "abbi")
+
+    assert_equal(len(tab.one_of(["rabbit", lambda x: x.value == "sir"])), 2)
 
     tab.debug_dimensions()
     return obs
--- test/test.py
+++ test/test.py
@@ -2,17 +2,19 @@ import databaker.bake as bake
 import unittest
 import warnings
 import imp
+from databaker.utf8csv import UnicodeReader
+
 warnings.simplefilter("ignore")
 
 class Options(object):
     def __init__(self, recipe, xls):
         self.xls_files = ['test/' + xls]
         self.recipe_file = 'test/' + recipe
-        self.timing = False
-        self.preview = False
-        self.preview_filename = "test/t_out.xls"
-        self.csv_filename = "test/t_out.csv"
-        self.csv = False
+        self.timing = True
+        self.preview = True
+        self.preview_filename = "t_out.xls"
+        self.csv_filename = "t_out.csv"
+        self.csv = True
 
 class datematch(unittest.TestCase):
     def test_datematch(self):
@@ -29,3 +31,30 @@ class normal_run(unittest.TestCase):
         recipe = imp.load_source("recipe", bake.Opt.recipe_file)
         for fn in bake.Opt.xls_files:
             bake.per_file(fn, recipe)
+        with open("test/t_out.csv") as f:
+            raw = list(UnicodeReader(f))
+        self.assertEqual(raw[0][0], 'observation')
+        self.assertEqual(raw[-1][0], '*'*9)
+        self.assertEqual(raw[-1][1], '9')
+        data = zip(*raw[1:-1])  # transpose
+        setdata = [set(x) for x in data]
+        print list(enumerate(setdata))
+        self.assertEqual(set(['Jan 2001', '2010', 'Jan-Mar 2005']), setdata[17])
+        self.assertEqual(setdata[18], setdata[17])
+        self.assertEqual(set(['Year', 'Quarter', 'Month']), setdata[19])
+
+        self.assertEqual(set(["header_1"]), setdata[35])
+        self.assertEqual(set(["ref"]), setdata[35+8])
+        self.assertEqual(set(["table"]), setdata[35+8+8])
+
+        self.assertEqual(setdata[38], set(["static_value"]))
+        self.assertEqual(setdata[38+8], set(x.upper() for x in data[1]))
+        self.assertEqual(setdata[38+8+8], set(["Sheet1"]))
+
+
+
+
+
+
+
+
