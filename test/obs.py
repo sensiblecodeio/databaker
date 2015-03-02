86846448d786e4cda6c9ae16af34fa7cdd3f283a
Dragon Dave McKee <dragon@scraperwiki.com>
2015-03-02 10:44:33 +0000

    tests

---
+++ test/obs.py
@@ -0,0 +1,14 @@
+import databaker.bake
+from nose.tools import assert_equal
+def per_file(tabs):
+    return tabs.names
+
+def per_tab(tab):
+    tab.dimension("foo", "bar")
+    parse = lambda x: databaker.bake.parse_ob(tab.excel_ref(x))
+    assert_equal(parse("C3"), ('2015', ''))
+    assert_equal(parse("C4"), ('2015', ''))
+    assert_equal(parse("C5"), ('2015', ''))
+    assert_equal(parse("E3"), ('2015', 'b'))
+    assert_equal(parse("G3"), ('2015', 'a'))
+    return tab
