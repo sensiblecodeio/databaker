7eede4e52218bc94f78e44fdd1fceee42557f27e
Julian Todd <julian@goatchurch.org.uk>
2016-12-01 13:23:37 +0000

    Assertions on the table names selections

--- databaker/framework.py
+++ databaker/framework.py
@@ -1,6 +1,6 @@
 import xlutils, xypath
 import databaker
-import os
+import os, warnings
 import databaker.constants
 from databaker.constants import *      # also brings in template
 import databaker.databakersolo as ds   # causes the xypath.loader to be overwritten
@@ -10,7 +10,18 @@ def loadxlstabs(inputfile, sheetids="*"):
     print("Loading %s which has size %d bytes" % (inputfile, os.path.getsize(inputfile)))
     tableset = xypath.loader.table_set(inputfile, extension='xls')
     tabs = list(xypath.loader.get_sheets(tableset, sheetids))
-    print("Table names: %s" % ", ".join([tab.name  for tab in tabs]))
+    tabnames = [ tab.name  for tab in tabs ]
+    print("Table names: %s" % str(tabnames))
+    
+    if sheetids != "*":
+        if type(sheetids) == str:
+            sheetids = [sheetids]
+        assert type(sheetids) in [list, tuple], ("What type is this?", type(sheetids))
+        for sid in sheetids:
+            assert sid in tabnames, (sid, "missing from found tables")
+        assert len(sheetids) == len(tabnames), ("Number of selected tables disagree", "len(sheetids) == len(tabnames)", len(sheetids), len(tabnames))
+    if len(set(tabnames)) != len(tabnames):
+        warnings.warn("Duplicates found in table names list")
     return tabs
     
 import pandas as pd
