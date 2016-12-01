4c0173263b59b9f2940c70a362ced8bfeb26c4f3
Julian Todd <julian@goatchurch.org.uk>
2016-12-01 12:48:57 +0000

    Add table sheet selection capabilit

--- databaker/framework.py
+++ databaker/framework.py
@@ -6,10 +6,10 @@ from databaker.constants import *      # also brings in template
 import databaker.databakersolo as ds   # causes the xypath.loader to be overwritten
 from databaker.jupybakeutils import HDim, HDimConst, savepreviewhtml, writetechnicalCSV, ConversionSegment
 
-def loadxlstabs(inputfile):
+def loadxlstabs(inputfile, sheetids="*"):
     print("Loading %s which has size %d bytes" % (inputfile, os.path.getsize(inputfile)))
     tableset = xypath.loader.table_set(inputfile, extension='xls')
-    tabs = list(xypath.loader.get_sheets(tableset, "*"))
+    tabs = list(xypath.loader.get_sheets(tableset, sheetids))
     print("Table names: %s" % ", ".join([tab.name  for tab in tabs]))
     return tabs
     
