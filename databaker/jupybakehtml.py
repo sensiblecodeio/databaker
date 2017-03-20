f1bb58a53ac46891639cd3e7b1b88b93bca6e91d
Julian Todd <julian@goatchurch.org.uk>
2017-03-20 09:55:13 +0000

    overcome rare missing font problem hit by savehtml rendering

--- databaker/jupybakehtml.py
+++ databaker/jupybakehtml.py
@@ -84,13 +84,15 @@ def tabletohtml(tab, tsubs, consolidatedcellvalueoverride, blocalstylesheet):
             if blocalstylesheet:
                 cs = [ ]
                 if ih is not None:             cs.append("xc%s" % ih)
-                if c.properties.get_bold():    cs.append("xb")
+                if c.properties.cell.sheet.book.font_list:  # overcome bug in messytables caused by https://www.communities-ni.gov.uk/sites/default/files/publications/communities/ni-housing-stats-15-16-tables1.xlsx
+                    if c.properties.get_bold():    cs.append("xb")
                 if c.is_number():              cs.append("xn")
                 htm.append('<td class="%s" title="%d %d">' % (" ".join(cs), c.x, c.y))
             else:
                 ls = [ ]
                 if ih is not None:             ls.append("background-color:%s" % colourlist.get(ih,"white"))
-                if c.properties.get_bold():    ls.append("font-weight:bold")
+                if c.properties.cell.sheet.book.font_list:
+                    if c.properties.get_bold():    ls.append("font-weight:bold")
                 lss = ' style="%s"' % ";".join(ls)  if ls  else ''
                 htm.append('<td%s title="%d %d">' % (lss, c.x, c.y))
                 
